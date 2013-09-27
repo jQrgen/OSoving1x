034b0adecd19b0591fb5a1e650fe15dd2d7c9479
jQrgen <jorgen.notland@gmail.com>
2013-09-27 11:05:12 +0200

    EatingArea

---
+++ src/EatingArea.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EatingArea {
+    private LinkedList seats = new LinkedList();
+    private final int capacity = 10;
+    Door door;
+    private Object lock = new Object();
+    
+    public EatingArea(Door door){
+    this.door = door;
+    
+    }
+    
+    public void AddToEatingArea() {
+        while(true){
+            synchronized(lock){
+                while(seats.size() == 10){ 
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(EatingArea.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            seats.add(door.GetCustomerInFrontOfTheLine());
+            }
+        }
+    }
+    
+    public void CostumerLeft(Customer customer) {
+        synchronized(lock){
+            seats.remove(customer);
+        }
+    }
+    
+}
